e22466496ca3ea07bfabec59edbbd2731badfb44
Abkarov Shamil <126020889+AbShamilDev@users.noreply.github.com>
2025-02-23 16:04:19 +0300

    feat: add new colors

--- src/components/WeatherWidget/WeatherWidget.tsx
+++ src/components/WeatherWidget/WeatherWidget.tsx
@@ -36,7 +36,7 @@ const intervals = {
   8: 0,
 };
 
-export const colors = ["#fad400", "#14da9e", "#932ac4", "#2a0fc2"];
+export const colors = ["#fad400", "#14da9e", "#ff6384", "#36a2eb", "#cc65fe"];
 
 function WeatherWidget() {
   const [city, setCity] = useState<string>("");
